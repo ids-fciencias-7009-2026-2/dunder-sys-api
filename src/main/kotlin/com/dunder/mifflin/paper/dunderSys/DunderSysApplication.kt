165d2b519518da2632076769b909fc33ddf3c2a2
LuisBerna <luis_berna@ciencias.unam.mx>
2026-02-24 00:12:08 -0600

    add DB connection

--- src/main/kotlin/com/dunder/mifflin/paper/dunderSys/DunderSysApplication.kt
+++ src/main/kotlin/com/dunder/mifflin/paper/dunderSys/DunderSysApplication.kt
@@ -1,5 +1,6 @@
 package com.dunder.mifflin.paper.dunderSys
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,5 +9,8 @@ class DunderSysApplication
 
 // Comentariooooo
 fun main(args: Array<String>) {
+	dotenv().entries().forEach {
+		System.setProperty(it.key, it.value)
+	}
 	runApplication<DunderSysApplication>(*args)
 }
